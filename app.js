ce00afb43bd60962a25a95729765d6a8e5940423
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-10-24 13:12:51 -0700

    test if it will break

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.get('/', (req, res) => {
-    res.send("GET THE DRUGGGSSS")
+    res.send("GET THE DRUGGGSSS ALEXXXXX")
 })
 
 // Route to test the connection
