075292b6ccf563128fd8d53aa047b9f645379fb9
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-11-05 15:12:59 -0800

    refactor of variable name LobbyID and LobbyCode --> lobbyId, wrote and integrated the function checkDbconnectionandIfLobbyExists() into firestoreServices.js

--- services/firestoreService.js
+++ services/firestoreService.js
@@ -2,18 +2,24 @@
 const { admin, db } = require('../configs/firebaseConfig') 
 const { documentSchema } = require('../models/lobbyModel')
 
-async function checkIfLobbyExists(lobbyID) {
+//check if the lobby exists in the db 
+async function checkDbconnectionandIfLobbyExists(lobbyId) {
     try {
+
+        if(!db) {
+            console.error("Firestore instance is not initialized")
+        } 
+
         //get the doc ref 
-        let lobbyDocRef = db.collection('lobbies').doc(lobbyID)
+        let lobbyDocRef = db.collection('lobbies').doc(lobbyId)
         let doc = await lobbyDocRef.get() 
         
         if(!doc.exists) {
-            console.error(`function checkIfLobbyExists: Lobby ${lobbyID} not found`)
+            console.error(`function checkIfLobbyExists: Lobby ${lobbyId} not found`)
             return false; 
         }
         
-        console.log(`function checkIfLobbyExists: Lobby ${lobbyID} found!`)
+        console.log(`function checkIfLobbyExists: Lobby ${lobbyId} found!`)
 
         return true
 
@@ -24,15 +30,17 @@ async function checkIfLobbyExists(lobbyID) {
 }
 
 //creating a document in firestore for lobby 
-async function createLobby(lobbyCode) {
-    if(!db) {
-        console.error("Firestore instance is not initialized")
-    } 
+//called inside inside of routes 
+async function createLobby(lobbyId) {
+
+    if(!checkDbconnectionandIfLobbyExists(lobbyId)) {
+        console.error("Could not find Lobby")
+    }
 
     try {
         let lobbies =  db.collection('lobbies')
-        await lobbies.doc(lobbyCode).set(documentSchema(lobbyCode))
-        console.log(`lobby created succesfully with join code ${lobbyCode}`)
+        await lobbies.doc(lobbyId).set(documentSchema(lobbyId))
+        console.log(`lobby created succesfully with join code ${lobbyId}`)
 
     } catch(error) {
         console.error("Error while creating lobby:", error)
@@ -40,19 +48,18 @@ async function createLobby(lobbyCode) {
 }
 
 //deleting a lobby in firestore
+//called in a route 
 async function deleteLobby(lobbyId) {
     console.log(`Attemping to deleteLobby: ${lobbyId}`)
         
+    if(!checkDbconnectionandIfLobbyExists(lobbyId)) {
+        console.error("Could not find Lobby")
+        return { success: false, message: `Lobby: ${lobbyId} does not exist or an error occured while trying to find it`}
+    }
+
     try {
         let lobbies = db.collection('lobbies')
         let document = lobbies.doc(lobbyId)
-
-        let documentExists = await document.get() 
-
-        if(!documentExists.exists) {
-            console.error(`document ${LobbyId} does not exist`)
-            return { success: false, message: `Lobby: ${LobbyId} does not exist`}
-        }
 
         //deleteTheLobby
         await document.delete()
@@ -71,9 +78,9 @@ async function addUserToLobby(lobbyId, UID) {
         let docRef = db.collection('lobbies').doc(lobbyId)
         
         //check if document exists 
-        let snapshot = await docRef.get() 
-        if(!snapshot.exists) {
-            return {success: false, message: `Lobby: ${lobbyId} does not exists`}
+        if(!checkDbconnectionandIfLobbyExists(lobbyId)) {
+            console.error("Could not find Lobby")
+            return { success: false, message: `Lobby: ${lobbyId} does not exist or an error occured while trying to find it`}
         }
 
         //add user to the users field of lobby 
@@ -90,19 +97,19 @@ async function addUserToLobby(lobbyId, UID) {
 }
 
 // TODO: implmeent single user removal and if they are the last user then also delete the lobby 
-async function removeUserFromLobby(lobbyID, UID) {
+async function removeUserFromLobby(lobbyId, UID) {
 
     //check if the user exists
 
 
     //get the document / lobby 
     try { 
-        let docRef = db.collection('lobbies').doc(lobbyID)
+        let docRef = db.collection('lobbies').doc(lobbyId)
         let snapshot = await docRef.get()
 
         //if the document / lobby does not exist 
         if(!snapshot.exists) {
-            return {success: false, message: `Lobby: ${lobbyID} does not exists`}
+            return {success: false, message: `Lobby: ${lobbyId} does not exists`}
         }
     } catch(e) {
         console.log(`An error occured while trying to retreive the document/lobby error: ${e}`)
@@ -117,8 +124,8 @@ async function removeUserFromLobby(lobbyID, UID) {
     
     //if the function is called by the last user then delete the document 
     if(user_array.length == 1) {
-        await deleteLobby(lobbyID)
-        return {success: true, message: `Lobby ${lobbyID} has been removed by the last user`}
+        await deleteLobby(lobbyId)
+        return {success: true, message: `Lobby ${lobbyId} has been removed by the last user`}
     }
 
     //else delete the user that called the api 
@@ -126,10 +133,10 @@ async function removeUserFromLobby(lobbyID, UID) {
         await docRef.update({
             users: admin.firestore.FieldValue.arrayRemove(UID)
         })
-        return {success: true, message: `User ${UID} has been removed from Lobby: ${lobbyID}`}
+        return {success: true, message: `User ${UID} has been removed from Lobby: ${lobbyId}`}
 
     } catch(error) {
-        return {success: false, message: `An error occured when trying to remove the user: ${UID} from Lobby: ${LobbyId} error: ${error}`}
+        return {success: false, message: `An error occured when trying to remove the user: ${UID} from Lobby: ${lobbyId} error: ${error}`}
     }
 }
 
@@ -137,7 +144,7 @@ async function removeUserFromLobby(lobbyID, UID) {
 
 async function test() {
     await createLobby("12455")
-    console.log(checkIfLobbyExists(12455))
+    console.log(await checkDbconnectionandIfLobbyExists("12455"))
     await addUserToLobby("12455", "TEST USER")
     await deleteLobby("12455")
 }
