8e8c5a846b3be11c72e14a226d5b1c0c45612e55
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-11-06 15:10:12 -0800

    added comments and formatting to firestoreRoutes.js

--- routes/firestoreRoutes.js
+++ routes/firestoreRoutes.js
@@ -1,14 +1,38 @@
-
-const firestoreController = require('../controllers/firestoreControllers')
-
-const express = require('express')
+//import neccesary modules and controllers
+const express = require("express");
 const router = express.Router();
+const firestoreController = require("../controllers/firestoreControllers");
 
+/**
+ * @route POST /firestore/createLobby
+ * @desc Create a new lobby with a specified ID returns lobby
+ * @access PUBLIC
+ */
+router.post("/createLobby", firestoreController.createLobby);
 
-// /firestore/
-router.post('/createLobby', firestoreController.createLobby) 
-router.delete('/deleteLobby', firestoreController.deleteLobby) 
-router.put('/addUserToLobby', firestoreController.addUserToLobby)
-router.delete('/removeUsersFromLobby', firestoreController.removeUsersFromLobby)
+/**
+ * @route DELETE /firestore/deleteLobby
+ * @desc Removes lobby from firestore db
+ * @access PUBLIC
+ */
+router.delete("/deleteLobby", firestoreController.deleteLobby);
 
-module.exports = router
+/**
+ * @route DELETE /firestore/addUserToLobby
+ * @desc adds user to firestore lobby document
+ * @access PUBLIC
+ */
+router.put("/addUserToLobby", firestoreController.addUserToLobby);
+
+/**
+ * @route DELETE /firestore/removeUsersFromLobby
+ * @desc Removes user from lobby or deletes lobby if last user in lobby calls
+ * @access PUBLIC
+ */
+router.delete(
+  "/removeUsersFromLobby",
+  firestoreController.removeUsersFromLobby
+);
+
+//export router to app.js
+module.exports = router;
