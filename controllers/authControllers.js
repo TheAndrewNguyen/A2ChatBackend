1006fe2d38050c4583d8ae4503839d3235e0d264
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-11-07 07:34:14 -0800

    removed uncessary file and changed name for lobbyID --> lobbyId and UID --> uid for api calls

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,18 +3,18 @@
 const authServices = require('../services/authServices')
 
 const deleteUser = async(req, res) => {
-    const {UID} = req.body
+    const {uid} = req.body
 
     try {
-        const result = await authServices.authDeleteUser(UID)
+        const result = await authServices.authDeleteUser(uid)
         if(result.success) {
-            res.status(200).json({"message": `user ${UID} was succesfully deleted`})
+            res.status(200).json({"message": `user ${uid} was succesfully deleted`})
         } else {
             throw new Error(result.message)
         }
     } catch(error) {
-        console.error(`An error occured while deleting user ${UID} from auth databse: ${error}`)
-        res.status(500).json( { "message": `An error occured while deleting user ${UID} from auth database`})
+        console.error(`An error occured while deleting user ${uid} from auth databse: ${error}`)
+        res.status(500).json( { "message": `An error occured while deleting user ${uid} from auth database`})
     }
 }
 
