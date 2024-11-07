67eecb12788f238a2ec7430ed76e52f3f9a53a6c
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-11-06 16:24:32 -0800

    implmeneted systemDataController.js for route /systemData

--- routes/systemDataRoutes.js
+++ routes/systemDataRoutes.js
@@ -1,48 +1,22 @@
 
-const { getData } = require('../utils/osUtils.js')
+
+const osUtils = require('../utils/osUtils.js')
 const express = require('express');
-const { fstat } = require('node:fs');
+const { fstat, symlinkSync } = require('node:fs');
 const path = require('node:path');
 const fs = require('fs') 
 
 const router = express.Router();
+const systemDataController = require('../controllers/systemDataController')
 
-router.get('/getData', async (req, res) => {
-    try {
-        let data = await getData()
-        res.send(data)  
-    } catch(e) {
-        console.error('error fetching data: ', e)
-    }
-})
+router.get('/getData', systemDataController.getData)
 
 //reads log files
-router.get('/log', (req, res) => {
-    let filePath = path.join('/home/ubuntu/.pm2/logs/app-out.log')
-    fs.readFile(filePath, 'utf-8', (err, data) =>{
-        if(err) {
-            console.error('Error reading log file:', err);
-            return res.status(500).send('Error reading log file');    
-        }
+router.get('/log', systemDataController.log);
 
-        res.setHeader('Content-Type', 'text/plain')
-        res.send(data)
-    })
-})
 
 //error log file
-router.get('/errorLog', (req, res) => {
-    let filePath = path.join('/home/ubuntu/.pm2/logs/app-error.log')
-    fs.readFile(filePath, 'utf-8', (err, data) =>{
-        if(err) {
-            console.error('Error reading log file:', err);
-            return res.status(500).send('Error reading log file');    
-        }
-
-        res.setHeader('Content-Type', 'text/plain')
-        res.send(data)
-    })
-})
+router.get('/errorLog', systemDataController.errorLog)
 
 
 module.exports = router
