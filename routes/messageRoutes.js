d2257424ea6157fa23a7e240100bd21a3cd14ae4
KamiKemiKimi <alexkim280@gmail.com>
2024-11-08 08:45:45 -0800

    completed message logic

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -9,4 +9,11 @@ const messageController = require('../controllers/messageControllers')
  */
  router.post("/:lobbyCode", messageController.handleSendMessage);
 
+ /**
+ * @route GET /messages/:lobbyCode
+ * @desc Get all messages from specified lobby id
+ * @access PUBLIC
+ */
+router.get("/:lobbyCode", messageController.handleGetMessage);
+
  module.exports = router;
--- services/messageService.js
+++ services/messageService.js
@@ -2,7 +2,6 @@ const { realtimeDb } = require("../configs/firebaseConfig");
 
 const sendMessage = async (messageContent, userId, timestamp, lobbyCode) => {
   try {
-    console.log('1')
     const ref = realtimeDb.ref('messages/' + lobbyCode);
     const newMessageRef = ref.push();
     await newMessageRef.set({
@@ -15,4 +14,27 @@ const sendMessage = async (messageContent, userId, timestamp, lobbyCode) => {
   }
 };
 
-module.exports = { sendMessage };
+const getMessages = async (lobbyCode) => {
+  try {
+    const ref = realtimeDb.ref('messages/' + lobbyCode);
+    const messages = [];
+
+    // Return a promise that resolves with the sorted messages
+    return new Promise((resolve, reject) => {
+      ref.orderByChild('timestamp').once('value', (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          messages.push(childSnapshot.val());
+        });
+        resolve(messages);  // Resolve with the sorted messages
+      }, (error) => {
+        reject(error);  // Reject in case of an error
+      });
+    });
+  } catch (error) {
+    console.error("Error fetching messages", error);
+    throw error;
+  }
+};
+
+
+module.exports = { sendMessage, getMessages };
