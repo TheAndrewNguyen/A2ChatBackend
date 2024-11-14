a25082d45f97974eb7d57267673103969d837ed5
KamiKemiKimi <alexkim280@gmail.com>
2024-11-14 12:18:52 -0800

    working jwt tokens

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,13 @@
 
 const express = require('express')
 const router = express.Router();
-const authController = require('../controllers/authControllers')
+const authController = require("../controllers/authControllers")
+const middleware = require('../middleware/verifyUser')
 
 // Endpoint to delete a user
-router.delete('/deleteUser/:uid', authController.deleteUser)
+router.delete('/deleteUser/:uid', middleware.verifyUser, authController.deleteUser)
+
+// Endpoint to verify user
+router.post('/verifyToken', middleware.verifyUser, authController.verifyToken)
 
 module.exports = router
--- src/routes/firestoreRoutes.js
+++ src/routes/firestoreRoutes.js
@@ -2,36 +2,35 @@
 const express = require("express");
 const router = express.Router();
 const firestoreController = require("../controllers/firestoreControllers");
+const middleware = require('../middleware/verifyUser');
 
 /**
  * @route POST /firestore/createLobby
  * @desc Create a new lobby with a specified ID returns lobby
  * @access PUBLIC
  */
-router.post("/createLobby", firestoreController.createLobby);
+router.post("/createLobby", middleware.verifyUser, firestoreController.createLobby);
 
 /**
  * @route DELETE /firestore/deleteLobby
  * @desc Removes lobby from firestore db
  * @access PUBLIC
  */
-router.delete("/deleteLobby", firestoreController.deleteLobby);
+router.delete("/deleteLobby", middleware.verifyUser, firestoreController.deleteLobby);
 
 /**
  * @route DELETE /firestore/addUserToLobby
  * @desc adds user to firestore lobby document
  * @access PUBLIC
  */
-router.put("/addUserToLobby", firestoreController.addUserToLobby);
+router.put("/addUserToLobby", middleware.verifyUser, firestoreController.addUserToLobby);
 
 /**
  * @route DELETE /firestore/removeUsersFromLobby
  * @desc Removes user from lobby or deletes lobby if last user in lobby calls
  * @access PUBLIC
  */
-router.delete(
-  "/removeUsersFromLobby/:lobbyId/:uid",
-  firestoreController.removeUsersFromLobby
+router.delete("/removeUsersFromLobby/:lobbyId/:uid", middleware.verifyUser, firestoreController.removeUsersFromLobby
 );
 
 //export router to app.js
